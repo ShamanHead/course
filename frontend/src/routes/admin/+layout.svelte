6e5f17a46b1496c08199cf436afb6581b46fb854
Arseniy <ronyplay247@gmail.com>
2023-05-10 05:03:00 +0300

    Remove backend, add some new pages!

---
+++ frontend/src/routes/admin/+layout.svelte
@@ -0,0 +1,14 @@
+<nav class="navigation">
+    <div class="logo">
+        MegaPochta
+    </div>
+    <div class="routes">
+        <a class="route" href="/">Домой</a>
+        <a class="route" href="/about">О нас</a>
+        <a class="route" href="/settings">Настройки</a>
+    </div>
+</nav>
+<script>
+    import "../styles/main.css";
+</script>
+<slot></slot>
